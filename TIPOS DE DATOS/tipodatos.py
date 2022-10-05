87bea3270da78671c0714e776979cc0495f95697
DIEGO <diegos.salazar@ayacucho.edu.pe>
2022-10-05 12:23:03 -0500

    tipos de datos

---
+++ TIPOS DE DATOS/tipodatos.py
@@ -0,0 +1,6 @@
+## tipos de datos numericos en python
+## los tipos numericos de python son tres int,float,complex
+5
+##son numeros enteros
+5.2 ##son numeros en coma flotante
+8.55e ##numeros complejos
