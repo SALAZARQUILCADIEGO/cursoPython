26965971c891d66e3e0ecc77330d31576c96ea32
DIEGO <diegos.salazar@ayacucho.edu.pe>
2022-10-09 15:21:18 -0500

    4 operaciones basicas

---
+++ TIPOS DE DATOS/operadores.py
@@ -0,0 +1,22 @@
+## operaciones suma,resta,multiplicacion,divicion.
+##suma
+numero_uno = 10
+numero_dos = 5
+suma = numero_uno +  numero_dos
+print("la suma de los dos valores es : " , suma)
+##resta.
+num1 = 20
+num2 = 5
+resta = num1-num2
+print("la resta de los dos valores  es: " , resta)
+##multiplicacion.
+numero1 = 20
+numero2 = 5
+multiplicacion = numero1 * numero2
+print("la multiplicacion de los dos numeros es: ", multiplicacion)
+##divicion.
+numero_1 = 20
+numero_2 = 5
+divicion = numero_1 / numero_2 
+print("la divicion de los dos numeros es: " , divicion)
+##son las 4 operaciones basicas.
