bcd28ef77a804562be7735cc97e497ec14bbdc8b
DIEGO <diegos.salazar@ayacucho.edu.pe>
2022-10-09 16:14:44 -0500

    ejemplos de for y while

---
+++ TIPOS DE DATOS/estructura_de_ciclo.py
@@ -0,0 +1,10 @@
+##El bucle for, en Python, es aquel que nos permitira iterar sobre una variable compleja, del tipo lista o tupla:
+## / Por cada nombre en mi_lista, imprimir nombre.
+mi_lista = ['Juan', 'Antonio', 'Pedro', 'Herminio'] 
+for nombre in mi_lista: 
+    print nombre
+## Bucle while Este bucle, se encarga de ejecutar una misma accion "mientras que" una determinada condicion se cumpla.   
+anio = 2001 
+while anio <= 2012: 
+    print "Informes del Año", str(anio) 
+    anio += 1
