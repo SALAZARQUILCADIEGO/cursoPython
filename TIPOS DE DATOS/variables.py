44f476c329abfc7d9c97a2d23b3293c1236850db
DIEGO <diegos.salazar@ayacucho.edu.pe>
2022-10-09 14:29:43 -0500

    tipos de variables

---
+++ TIPOS DE DATOS/variables.py
@@ -0,0 +1,23 @@
+##variables son espacios y lo importante debe de ser exprecion para que sea entendible.
+## En python las dos variables mas comunes son las que almacenan numeros y textos.
+##las variables que almacenan numeros hay dos tipos principales .
+## los numeros enteros llamados (int).
+5 ##dato tipo entero.
+## los numeros decimales o reales llamados(float).
+9.55 ## dato tipo decimal.
+## varialles que almacenan texto, se les denomina strings(str).
+##Es muy importante, que el contenido a almacenar en estos tipo de variables, se encuentre entre comillas.
+"hola mundo" ## variable tipo strings.
+## declaracion de variables ejemplo
+print("esto es una suma")
+numero_uno =2
+numero_dos =4.5
+resultado = numero_uno + numero_dos
+print(resultado)
+##especificacion de todo tipo de variables.
+c ## caracter.
+"cadenas" ##cadena de caracteres.
+8 ## numeros enteros , decimales y reales.
+false ## tipo de dato booleano
+true ## tipo de dato booleanos
+
