d6d9de8cb2e6932668e87f48223683700b54828b
DIEGO <diegos.salazar@ayacucho.edu.pe>
2022-10-09 15:54:28 -0500

    tipos de estructuras de decision

---
+++ TIPOS DE DATOS/estrcutura de decicion.py
@@ -0,0 +1,21 @@
+## estrructura de decision  if/elif/else 
+## if realiza una accion especifica solo cuando la condicion es verdadera; de lo contrario, la accion se ignora.
+## La estructura if/else permite al programador especificar que una accion diferente se debe desarrollar
+## cuando la condicion es falsa.
+
+## ejemplo
+ if nota >= 60:
+      print ('Aprobado')
+   else:
+      print ('Suspendido')
+##Tambien se utiliza la estructura de seleccion multiple if/elif/else. Que se veria en Python de la siguiente manera:
+if nota >= 90:
+      print ('A')
+   elif nota >= 80:
+      print ('B')
+   elif nota >= 70:
+      print ('C')
+   elif nota >= 60:
+      print ('D')
+   else:
+      print ('F')
