771bd81fc2096630b06bf9eab48299235a51dc19
DIEGO <diegos.salazar@ayacucho.edu.pe>
2022-10-12 11:47:58 -0500

    trabajo con while2

---
+++ ESTRUCTURA DE CICLO/trabajo en clase2.py
@@ -0,0 +1,7 @@
+condicion=True
+while condicion==True:
+    EDAD=int(input("ingresa su edad"))
+    if EDAD==20:
+        print("su edad es correcta")
+    else:
+        print("vuelva a introducir su edad")
