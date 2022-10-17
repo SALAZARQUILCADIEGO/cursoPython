751e0b64a24a70a27d18d1ebf0c5716ea782f32e
DIEGO <diegos.salazar@ayacucho.edu.pe>
2022-10-17 09:51:55 -0500

    convirtiendo de  match a if - elif-else

--- ESTRUCTURA DE DECICION/estrcutura de decicion.py
+++ ESTRUCTURA DE DECICION/estrcutura de decicion.py
@@ -4,18 +4,18 @@
 ## cuando la condicion es falsa.
 
 ## ejemplo
- if nota >= 60:
-      print ('Aprobado')
-   else:
-      print ('Suspendido')
+if nota >= 60:
+   print ("Aprobado")
+else:
+      print ("Suspendido")
 ##Tambien se utiliza la estructura de seleccion multiple if/elif/else. Que se veria en Python de la siguiente manera:
 if nota >= 90:
-      print ('A')
-   elif nota >= 80:
-      print ('B')
-   elif nota >= 70:
-      print ('C')
-   elif nota >= 60:
-      print ('D')
-   else:
-      print ('F')
+   print ("A")
+elif nota >= 80:
+   print ("B")
+elif nota >= 70:
+   print ("C")
+elif Nota >= 60:
+   print ("D")
+else:
+  print ("F")
--- ESTRUCTURA DE DECICION/match.py
+++ ESTRUCTURA DE DECICION/match.py
@@ -1,11 +1,37 @@
+#tendremos una variable con el mensaje hola mundo
+#pedimos al usuario un texto
+#si el texto ingresado es hola
+#mostrara el mensaje completo
+#si el texto ingresado es como estas
+#mostrara lka palabra hola
+#si el mensjae es saludos 
+#mostrara la paalabra mundo
+#si engresa otras letras 
+#mostrara error
+# mensaje="hola mundo"
+# texto=input("ingresa un texto :")
+# match texto:
+#     case "hola":
+#      print (mensaje[:])
+#     case "como estas":
+#         print(mensaje[:4])
+#     case "saludos":
+#         print(mensaje[5:])
+#     case _:
+#         print("error")
+
+# comvirtiendo match a if 
+
+from distutils.log import error
+
+
 mensaje="hola mundo"
-texto=input("ingresa un texto :")
-match texto:
-    case "hola":
-     print (mensaje[:])
-    case "como estas":
-        print(mensaje[:4])
-    case "saludos":
-        print(mensaje[5:])
-    case _:
-        print("error")
+texto=input("ingresa un texto")
+if texto == "hola":
+    print("hola mundo")
+elif texto =="como estas":
+    print("hola")
+elif texto=="saludos":
+    print("mundo")
+else:
+    print("error")
